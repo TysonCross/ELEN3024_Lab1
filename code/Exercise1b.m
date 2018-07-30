bb53e9fc9bcf77e26f40fa46812ba6410b909cfc
Tyson <tyson.cross@gmail.com>
2018-07-30 21:10:29 +0200

    Initial commit

---
+++ code/Exercise1b.m
@@ -0,0 +1,24 @@
+% ELEN3024 Lab 1 - Exercise 1b
+% Tyson Cross 1239448
+% Jason Parry 1046955
+% Rashaad Cassim 1099797
+
+
+%{
+    Notes and information
+%}
+
+clc; clear all; set(0,'ShowHiddenHandles','on'); delete(get(0,'Children'));
+
+% Calculations
+% go here...
+N = 1;
+x = [1:10];
+y = sin(x);
+y1 = cos(x);
+
+% Display results
+Exercise1b_Disp;
+
+% Plot results
+Exercise1b_Plot;
